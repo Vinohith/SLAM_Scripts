3c2db20a3bb04df69ca197c0f4fa1d05d5c68d3b
P Vinohith Reddy <vinohith1998@gmail.com>
2021-04-22 17:06:42 +0530

    Update triangulation.cpp and add comments

--- Triangulation/triangulation.cpp
+++ Triangulation/triangulation.cpp
@@ -4,7 +4,33 @@
 #include <opencv2/features2d.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/xfeatures2d.hpp>
 #include <vector>
+
+/*
+Step 1 : Extract features from image1 and Image2 and then match them (matches are from Image1(queryIdx) to Image2(trainIdx)).
+Step 2 : Estimate the 2D-2D pose between the two image frames (this produces the transformation of points in frame of Image1 
+															   to frame of Image2), which is the relative pose T_c2_c1.
+Step 3 : Triangulate the points using the matches (the output points will be in the in the reference frame of Image1)
+
+Example : If the frame of Image1 is the world frame, then performing 2D-2D pose estimation results in Tcw = [R|t] for Image2
+		  (i.e. the transformation of points in world frame to the frame of Image2 (current frame)). Because the triangulation
+		  is happening between points in Image1 and Image2 using Image1 frame as the reference frame (world frame) and Image2 
+		  frame (Tcw => can be considered as the pose of camera 2 in the world frame) the output triangulated points will be 
+		  in the frame of Image2 (i.e. world frame). p_c = K * Tcw * P_w
+
+		  If frame of Image1 is not the world frame, but some T_c1_w, then 2D-2D Pose estimation produces T_c2_c1, then,
+		  T_c2_w = T_c2_c1 * T_c1_w (the matrices should be converted to 4x4 matrices).
+
+In conclusion : 2D-2D Pose estimation produces the transformation of points in frame of Image1 to frame of Image2 (i.e. T_c2_c1
+				transformation of points in camera 1 to camera 2. This can be considered as the pose of camera 2 in the frame
+				of camera 1). p_c2 = K * T_c2_c1 * P_c1
+
+				Triangulating the matches in Image1 and Image2 using the poses of camera 1 (in some reference frame) and camera 2 
+				(in some ereference frame) results in producing the triangulated points in the reference frame (can be considered as
+				the world frame). So, if pose of camera 1 = T_c1_w and pose of camera 2 = T_c2_w, then the triangulated points will
+				be P_w, such that, p_c1 = K * T_c1_w * P_w and p_c2 = K * T_c2_w * P_w
+*/
 
 void find_feature_matches(const cv::Mat &img1, const cv::Mat &img2,
                           std::vector<cv::KeyPoint> &keypoints1,
@@ -20,20 +46,11 @@ void triangulate_points(std::vector<cv::KeyPoint> &keypoints1,
                         std::vector<cv::KeyPoint> &keypoints2,
                         std::vector<cv::DMatch> &matches, cv::Mat &K,
                         cv::Mat &R, cv::Mat &t,
-                        std::vector<cv::Point3d> &points);
+                        std::vector<cv::Point3f> &points);
 
-cv::Point2d pixel2cam(const cv::Point2d &p, const cv::Mat &K);
+cv::Point2f pixel2cam(const cv::Point2f &p, const cv::Mat &K);
 
-inline cv::Scalar get_color(float depth) {
-  float up_th = 50, low_th = 10, th_range = up_th - low_th;
-  if (depth > up_th) {
-    depth = up_th;
-  }
-  if (depth < low_th) {
-    depth = low_th;
-  }
-  return cv::Scalar(255 * depth / th_range, 0, 255 * (1 - depth / th_range));
-}
+
 
 int main(int argc, char **argv) {
   if (argc != 3) {
@@ -51,47 +68,64 @@ int main(int argc, char **argv) {
 
   cv::Mat R, t;
   cv::Mat K =
-      (cv::Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
+      (cv::Mat_<float>(3, 3) << 707.0912, 0, 601.8873, 0, 707.0912, 183.1104, 0, 0, 1);
   pose_estimation_2d2d(keypoints1, keypoints2, matches, K, R, t);
   std::cout << "R = " << R << std::endl;
   std::cout << "t = " << t << std::endl;
 
-  std::vector<cv::Point3d> points;
+  std::vector<cv::Point3f> points;
   triangulate_points(keypoints1, keypoints2, matches, K, R, t, points);
 
-  // creates independent copy
-  cv::Mat img1_plot = img1.clone();
-  cv::Mat img2_plot = img2.clone();
-  for (int i = 0; i < matches.size(); i++) {
-    float depth1 = points[i].z;
-    cv::Point2d pt_cam1 = pixel2cam(keypoints1[matches[i].queryIdx].pt, K);
-    cv::circle(img1_plot, keypoints1[matches[i].queryIdx].pt, 2,
-               get_color(depth1), 2);
-    // p_t = R*p + t
-    cv::Mat pt_trans =
-        R * (cv::Mat_<double>(3, 1) << points[i].x, points[i].y, points[i].z) +
-        t;
-    float depth2 = pt_trans.at<float>(2, 0);
-    cv::circle(img2_plot, keypoints2[matches[i].trainIdx].pt, 2,
-               get_color(depth2), 2);
-  }
+  // std::cout << points.size() << std::endl;
 
-  cv::imshow("img 1", img1_plot);
-  cv::imshow("img 2", img2_plot);
-  cv::waitKey(0);
+  for (int i=0; i < points.size(); i++){
+  	cv::DMatch match = matches[i];
+  	cv::Point2f pts1 = keypoints1[match.queryIdx].pt; 
+  	cv::Point2f pts2 = keypoints2[match.trainIdx].pt;
+  	cv::Mat P = (cv::Mat_<float>(4, 1) << points[i].x, points[i].y, points[i].z , (float) 1);
+  	cv::Mat T1 = (cv::Mat_<float>(3, 4) << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0);
+  	cv::Mat T2 = (cv::Mat_<float>(3, 4) << R.at<double>(0, 0), R.at<double>(0, 1),
+                R.at<double>(0, 2), t.at<double>(0, 0), R.at<double>(1, 0),
+                R.at<double>(1, 1), R.at<double>(1, 2), t.at<double>(1, 0),
+                R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2),
+                t.at<double>(2, 0));
+  	// cv::Mat T2 = (cv::Mat_<float>(3, 4) << 9.999910e-01, 1.048972e-03, -4.131348e-03, -9.374345e-02,
+  	// 									  -1.058514e-03, 9.999968e-01, -2.308104e-03, -5.676064e-02,
+  	// 									   4.128913e-03, 2.312456e-03, 9.999887e-01, 1.716275e+00);
+  	std::cout << points[i] << std::endl;
+  	// Pose 1
+  	cv::Mat p1 = K * T1 * P;
+  	p1 /= p1.at<float>(2, 0);
+  	cv::Point2f pt_cam1(p1.at<float>(0, 0), p1.at<float>(1, 0));
+  	std::cout << pts1 << "  " << pt_cam1 << std::endl;
+  	// Pose 2
+  	cv::Mat p2 = K * T2 * P;
+  	p2 /= p2.at<float>(2, 0);
+  	cv::Point2f pt_cam2(p2.at<float>(0, 0), p2.at<float>(1, 0));
+  	std::cout << pts2 << "  " << pt_cam2 << std::endl;
+
+  	std::cout << std::endl;
+  }
 
   return 0;
 }
+
 
 void find_feature_matches(const cv::Mat &img1, const cv::Mat &img2,
                           std::vector<cv::KeyPoint> &keypoints1,
                           std::vector<cv::KeyPoint> &keypoints2,
                           std::vector<cv::DMatch> &matches) {
 
-  cv::Ptr<cv::FeatureDetector> detector = cv::ORB::create();
+  cv::Ptr<cv::FeatureDetector> detector = cv::ORB::create(2000);
   cv::Ptr<cv::DescriptorExtractor> descriptor = cv::ORB::create();
   cv::Ptr<cv::DescriptorMatcher> matcher =
       cv::DescriptorMatcher::create("BruteForce-Hamming");
+  // cv::Ptr<cv::FeatureDetector> detector =
+  //     cv::GFTTDetector::create(1000, 0.001, 15.0);
+  // cv::Ptr<cv::DescriptorExtractor> descriptor =
+  //     cv::xfeatures2d::BriefDescriptorExtractor::create();
+  // cv::Ptr<cv::DescriptorMatcher> matcher =
+  //       cv::BFMatcher::create(cv::NORM_HAMMING);
 
   detector->detect(img1, keypoints1);
   detector->detect(img2, keypoints2);
@@ -107,18 +141,19 @@ void find_feature_matches(const cv::Mat &img1, const cv::Mat &img2,
                           [](const cv::DMatch &m1, const cv::DMatch &m2) {
                             return m1.distance < m2.distance;
                           });
-  double mindist = minmax.first->distance;
+  float mindist = minmax.first->distance;
   for (cv::DMatch match : allmatches) {
-    if (match.distance <= std::max(2 * mindist, 30.0)) {
+    if (match.distance <= std::max(2 * mindist, (float) 30.0)) {
       matches.push_back(match);
     }
   }
 
-  //   cv::Mat imgmatch;
-  //   cv::drawMatches(img1, keypoints1, img2, keypoints2, matches, imgmatch);
-  //   cv::imshow("Matches", imgmatch);
-  //   cv::waitKey(0);
+    cv::Mat imgmatch;
+    cv::drawMatches(img1, keypoints1, img2, keypoints2, matches, imgmatch);
+    cv::imshow("Matches", imgmatch);
+    cv::waitKey(0);
 }
+
 
 void pose_estimation_2d2d(std::vector<cv::KeyPoint> &keypoints1,
                           std::vector<cv::KeyPoint> &keypoints2,
@@ -131,8 +166,8 @@ void pose_estimation_2d2d(std::vector<cv::KeyPoint> &keypoints1,
     points2.push_back(keypoints2[match.trainIdx].pt);
   }
 
-  cv::Point2d principal_point(K.at<double>(0, 2), K.at<double>(1, 2));
-  double focal_length = K.at<double>(1, 1);
+  cv::Point2f principal_point(K.at<float>(0, 2), K.at<float>(1, 2));
+  float focal_length = K.at<float>(1, 1);
   cv::Mat essential_matrix =
       cv::findEssentialMat(points1, points2, focal_length, principal_point);
   cv::recoverPose(essential_matrix, points1, points2, K, R, t);
@@ -142,15 +177,19 @@ void triangulate_points(std::vector<cv::KeyPoint> &keypoints1,
                         std::vector<cv::KeyPoint> &keypoints2,
                         std::vector<cv::DMatch> &matches, cv::Mat &K,
                         cv::Mat &R, cv::Mat &t,
-                        std::vector<cv::Point3d> &points) {
-  cv::Mat T1 = (cv::Mat_<float>(3, 4) << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0);
-  cv::Mat T2 = (cv::Mat_<float>(3, 4) << R.at<double>(0, 0), R.at<double>(0, 1),
+                        std::vector<cv::Point3f> &points) {
+	cv::Mat T1 = (cv::Mat_<float>(3, 4) << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0);
+  	cv::Mat T2 = (cv::Mat_<float>(3, 4) << R.at<double>(0, 0), R.at<double>(0, 1),
                 R.at<double>(0, 2), t.at<double>(0, 0), R.at<double>(1, 0),
                 R.at<double>(1, 1), R.at<double>(1, 2), t.at<double>(1, 0),
                 R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2),
                 t.at<double>(2, 0));
+  	// cv::Mat T2 = (cv::Mat_<float>(3, 4) << 9.999910e-01, 1.048972e-03, -4.131348e-03, -9.374345e-02,
+  	// 									  -1.058514e-03, 9.999968e-01, -2.308104e-03, -5.676064e-02,
+  	// 									   4.128913e-03, 2.312456e-03, 9.999887e-01, 1.716275e+00);
   std::vector<cv::Point2f> pts1, pts2;
   for (cv::DMatch match : matches) {
+  	// std::cout << match.queryIdx << "  " << match.trainIdx << std::endl;
     pts1.push_back(pixel2cam(keypoints1[match.queryIdx].pt, K));
     pts2.push_back(pixel2cam(keypoints2[match.trainIdx].pt, K));
   }
@@ -159,17 +198,18 @@ void triangulate_points(std::vector<cv::KeyPoint> &keypoints1,
   std::cout << pts4d.size() << std::endl;
   std::cout << pts4d.cols << std::endl;
   for (int i = 0; i < pts4d.cols; i++) {
-    std::cout << "4D point : " << pts4d.col(i) << std::endl;
+    // std::cout << "4D point : " << pts4d.col(i) << std::endl;
     cv::Mat x = pts4d.col(i);
     x /= x.at<float>(3, 0);
-    std::cout << "3D point : " << x.at<float>(0, 0) << ", " << x.at<float>(1, 0)
-              << ", " << x.at<float>(2, 0) << std::endl;
-    cv::Point3d p(x.at<float>(0, 0), x.at<float>(1, 0), x.at<float>(2, 0));
+    // std::cout << "3D point : " << x.at<float>(0, 0) << ", " << x.at<float>(1, 0)
+    //           << ", " << x.at<float>(2, 0) << std::endl;
+    cv::Point3f p(x.at<float>(0, 0), x.at<float>(1, 0), x.at<float>(2, 0));
     points.push_back(p);
   }
 }
 
-cv::Point2d pixel2cam(const cv::Point2d &p, const cv::Mat &K) {
-  return cv::Point2d((p.x - K.at<double>(0, 2)) / K.at<double>(0, 0),
-                     (p.y - K.at<double>(1, 2)) / K.at<double>(1, 1));
+
+cv::Point2f pixel2cam(const cv::Point2f &p, const cv::Mat &K) {
+  return cv::Point2f((p.x - K.at<float>(0, 2)) / K.at<float>(0, 0),
+                     (p.y - K.at<float>(1, 2)) / K.at<float>(1, 1));
 }
