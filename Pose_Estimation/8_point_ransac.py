1d9e57edc1740898d0d4185f91f931deb9f90c2c
P Vinohith Reddy <vinohith1998@gmail.com>
2021-06-10 11:56:54 +0530

    Add code to draw epipolar lines

--- Pose_Estimation/8_point_ransac.py
+++ Pose_Estimation/8_point_ransac.py
@@ -11,7 +11,8 @@ Output : Interest Points, Descriptors
 def compute_features(img):
     # detector = cv2.SIFT_create()
     # kp, des = detector.detectAndCompute(img,None)
-    detector = cv2.GFTTDetector_create(2000, useHarrisDetector=True)
+    detector = cv2.GFTTDetector_create(2000, qualityLevel=0.001, minDistance=1,
+                                       blockSize=3, useHarrisDetector=True, k=0.04)
     descriptor = cv2.xfeatures2d.BriefDescriptorExtractor_create()
     kp = detector.detect(img)
     kp, des = descriptor.compute(img, kp)
@@ -23,7 +24,7 @@ Function to compute the matching points b/w two images
 Input : Descriptors of Image1, Descriptors of Image2
 Output : List of matched point pairs (type : cv::DMatch)
 """
-def compute_matchers(des1, des2):
+def compute_matchers(kp1, kp2, des1, des2):
     # FLANN_INDEX_KDTREE = 1
     # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
     # search_params = dict(checks=50)
@@ -39,19 +40,19 @@ def compute_matchers(des1, des2):
     #         filtered_matches.append(cv2.DMatch(m.queryIdx, m.trainIdx, m.distance))
     # matched_kp1 = np.array(matched_kp1)
     # matched_kp2 = np.array(matched_kp2)
-    matcher = cv2.BFMatcher_create(cv2.NORM_HAMMING)
+    # # Use above one for SIFT features
+    matcher = cv2.BFMatcher_create(cv2.NORM_HAMMING, crossCheck=False)
     matches = matcher.match(des1, des2)
     matches = sorted(matches, key = lambda x:x.distance)
     mindist = matches[0].distance
     filtered_matches = []
-    for match in matches:
-        if (match.distance <= max(2*mindist, 50.0)):
-            filtered_matches.append(match)
     matched_kp1 = []
     matched_kp2 = []
     for match in matches:
-        matched_kp1.append(kp1[match.queryIdx].pt)
-        matched_kp2.append(kp2[match.trainIdx].pt)
+        if (match.distance <= max(2*mindist, 30.0)):
+            filtered_matches.append(match)
+            matched_kp1.append(kp1[match.queryIdx].pt)
+            matched_kp2.append(kp2[match.trainIdx].pt)
     matched_kp1 = np.array(matched_kp1)
     matched_kp2 = np.array(matched_kp2)
     return filtered_matches, matched_kp1, matched_kp2
@@ -144,7 +145,7 @@ def evaluate_F(kp1, kp2, estimate, thresh):
             img1_inliers.append(kp1[i])
             img2_inliers.append(kp2[i])
             inlier_count += 1
-    print("Inlier count : ", inlier_count)
+    # print("Inlier count : ", inlier_count)
     return (np.array(img1_inliers), np.array(img2_inliers), inlier_count)
 
 
@@ -175,169 +176,101 @@ def Ransac(kp1, kp2, num_iterations, thresh):
             best_img1_inlier_pts = img1_inlier_pts
             best_img2_inlier_pts = img2_inlier_pts
         ite += 1
-    return best_F_mat 
+    return best_F_mat, best_img1_inlier_pts, best_img2_inlier_pts
 
-
-
-np.random.seed(0)
-# K = np.array([[520.0, 0.0, 325.1], 
-#               [0.0, 521.0, 249.7],
-#               [0.0, 0.0, 1.0]])
-
-# img1 = cv2.imread("1.png", 0)
-# kp1, des1 = compute_features(img1)
-# img2 = cv2.imread("2.png", 0)
-# kp2, des2 = compute_features(img2)
-K = np.array([[718.8560,0.0,607.1928],[0.0,718.8560,185.2157],[0.0,0.0,1]])
-# img1 = cv2.imread("000000.png", 0)
-# kp1, des1 = compute_features(img1)
-# img2 = cv2.imread("000001.png", 0)
-# kp2, des2 = compute_features(img2)
-
-img1 = cv2.imread("left.jpg", 0)
-kp1, des1 = compute_features(img1)
-img2 = cv2.imread("right.jpg", 0)
-kp2, des2 = compute_features(img2)
-
-matches, matched_kp1, matched_kp2 = compute_matchers(des1, des2)
-
-F = Ransac(matched_kp1, matched_kp2, 50, 10.0)
-print(F)
-# F, mask = cv2.findFundamentalMat(matched_kp1, matched_kp2, cv2.FM_LMEDS)
-
-def drawlines(img1,lines):
+"""
+Draw the epipolar lines
+"""
+def drawlines(img1,lines, pts1):
     ''' img1 - image on which we draw the epilines for the points in img2
         lines - corresponding epilines '''
     r,c = img1.shape
     img1 = cv2.cvtColor(img1,cv2.COLOR_GRAY2BGR)
-    for r in lines:
-        # print(r[0])
+    for l,pt1 in zip(lines, pts1):
         color = tuple(np.random.randint(0,255,3).tolist())
-        x0,y0 = map(int, [0, -r[2]/r[1] ])
-        x1,y1 = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
+        x0,y0 = map(int, [0, -l[2]/l[1] ])
+        x1,y1 = map(int, [c, -(l[2]+(l[0]*c))/l[1] ])
         img1 = cv2.line(img1, (x0,y0), (x1,y1), color,1)
+        img1 = cv2.circle(img1,tuple(pt1),5,color,-1)
     return img1
 
 
-lines1 = cv2.computeCorrespondEpilines(matched_kp2.reshape(-1,1,2), 2, F)
-lines1 = lines1.reshape(-1, 3)
-img5 = drawlines(img1, lines1)
-lines2 = cv2.computeCorrespondEpilines(matched_kp1.reshape(-1,1,2), 1, F)
-lines2 = lines2.reshape(-1, 3)
-img6 = drawlines(img2, lines2)
-
-E = K.T @ F @ K
-U, S, Vt = np.linalg.svd(E)
-sigma = 1.0
-E = U @ np.diag([sigma, sigma, 0.0]) @ Vt
-print(E)
-# W = np.array([[0.0,-1.0,0.0], [1.0,0.0,0.0], [0.0,0.0,1.0]])
-# U, S, Vt = np.linalg.svd(E)
-# R = U @ W.T @ Vt
-# print(R)
-# t = U @ W @ np.diag(S) @ U.T
-# print(t)
-_, R, t, mask = cv2.recoverPose(E, matched_kp1, matched_kp2, K)
-print(R)
-print(t)
-
-E, mask = cv2.findEssentialMat(matched_kp1, matched_kp2, K, cv2.RANSAC)
-u, s, vt = np.linalg.svd(E)
-# print(u)
-# print(s)
-print(E)
-_, R, t, mask = cv2.recoverPose(E, matched_kp1, matched_kp2, K)
-print(R)
-print(t)
-# W = np.array([[0.0,-1.0,0.0], [1.0,0.0,0.0], [0.0,0.0,1.0]])
-# U, S, Vt = np.linalg.svd(E)
-# R = U @ W.T @ Vt
-# print(R)
-
-plt.subplot(121),plt.imshow(img5)
-plt.subplot(122),plt.imshow(img6)
-plt.show()
-
-outmatches = cv2.drawMatches(img1, kp1, img2, kp2, matches, np.array([]))
-# print(kp1[matches[0].queryIdx].pt, kp2[matches[0].trainIdx].pt)
-cv2.imshow("", outmatches)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-""" outmatches = cv2.drawMatches(img1, kp1, img2, kp2, matches, np.array([]))
-print(kp1[matches[0].queryIdx].pt, kp2[matches[0].trainIdx].pt)
-cv2.imshow("", outmatches)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-out1 = cv2.drawKeypoints(img1, kp1, np.array([]), color=(255,0,0))
-out2 = cv2.drawKeypoints(img2, kp2, np.array([]), color=(255,0,0))
-# print(np.array(kp1).size(), np.array(des1).size())
-# print(np.array(kp1).shape)
-cv2.imshow("", out1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imshow("", out2)
-cv2.waitKey(0)
-cv2.destroyAllWindows() """
+def main():
+    np.random.seed(0)
+    K = np.array([[520.0, 0.0, 325.1], 
+                [0.0, 521.0, 249.7],
+                [0.0, 0.0, 1.0]])
+    img1 = cv2.imread("1.png", 0)
+    kp1, des1 = compute_features(img1)
+    img2 = cv2.imread("2.png", 0)
+    kp2, des2 = compute_features(img2)
+    # K = np.array([[718.8560,0.0,607.1928],[0.0,718.8560,185.2157],[0.0,0.0,1]])
+    # img1 = cv2.imread("000000.png", 0)
+    # kp1, des1 = compute_features(img1)
+    # img2 = cv2.imread("000001.png", 0)
+    # kp2, des2 = compute_features(img2)
 
 
+    matches, matched_kp1, matched_kp2 = compute_matchers(kp1, kp2, des1, des2)
+    matched_kp1 = np.int32(matched_kp1)
+    matched_kp2 = np.int32(matched_kp2)
+    # print(matched_kp1.shape)
 
-# import numpy as np
-# import cv2 as cv
-# from matplotlib import pyplot as plt
-# img1 = cv.imread('000000.png',0)  #queryimage # left image
-# img2 = cv.imread('000001.png',0) #trainimage # right image
-# sift = cv.SIFT_create()
-# # find the keypoints and descriptors with SIFT
-# kp1, des1 = sift.detectAndCompute(img1,None)
-# kp2, des2 = sift.detectAndCompute(img2,None)
-# # FLANN parameters
-# FLANN_INDEX_KDTREE = 1
-# index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-# search_params = dict(checks=50)
-# flann = cv.FlannBasedMatcher(index_params,search_params)
-# matches = flann.knnMatch(des1,des2,k=2)
-# pts1 = []
-# pts2 = []
-# # ratio test as per Lowe's paper
-# for i,(m,n) in enumerate(matches):
-#     if m.distance < 0.8*n.distance:
-#         pts2.append(kp2[m.trainIdx].pt)
-#         pts1.append(kp1[m.queryIdx].pt)
+    F, best_matched_kp1, best_matched_kp2 = Ransac(matched_kp1, matched_kp2, 100, 10.0)
 
-# pts1 = np.int32(pts1)
-# pts2 = np.int32(pts2)
-# F, mask = cv.findFundamentalMat(pts1,pts2,cv.FM_LMEDS)
-# print(F)
-# # We select only inlier points
-# pts1 = pts1[mask.ravel()==1]
-# pts2 = pts2[mask.ravel()==1]
+    lines1 = cv2.computeCorrespondEpilines(best_matched_kp1.reshape(-1,1,2), 2, F)
+    lines1 = lines1.reshape(-1, 3)
+    img5 = drawlines(img1, lines1, best_matched_kp1)
+    lines2 = cv2.computeCorrespondEpilines(best_matched_kp2.reshape(-1,1,2), 1, F)
+    lines2 = lines2.reshape(-1, 3)
+    img6 = drawlines(img2, lines2, best_matched_kp2)
 
-# def drawlines(img1,img2,lines,pts1,pts2):
-#     ''' img1 - image on which we draw the epilines for the points in img2
-#         lines - corresponding epilines '''
-#     r,c = img1.shape
-#     img1 = cv.cvtColor(img1,cv.COLOR_GRAY2BGR)
-#     img2 = cv.cvtColor(img2,cv.COLOR_GRAY2BGR)
-#     for r,pt1,pt2 in zip(lines,pts1,pts2):
-#         color = tuple(np.random.randint(0,255,3).tolist())
-#         x0,y0 = map(int, [0, -r[2]/r[1] ])
-#         x1,y1 = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
-#         img1 = cv.line(img1, (x0,y0), (x1,y1), color,1)
-#         img1 = cv.circle(img1,tuple(pt1),5,color,-1)
-#         img2 = cv.circle(img2,tuple(pt2),5,color,-1)
-#     return img1,img2
+    # Extract E from F
+    E = K.T @ F @ K
+    U, S, Vt = np.linalg.svd(E)
+    sigma = 1.0
+    # E matrix should have equal singular values (using 1 to remove scale)
+    E = U @ np.diag([sigma, sigma, 0.0]) @ Vt
+    print("********* Scratch ***********")
+    print("E : ")
+    print(E)
+    """ 
+    # # Custom way of calculating R and t from E.
+    # W = np.array([[0.0,-1.0,0.0], [1.0,0.0,0.0], [0.0,0.0,1.0]])
+    # U, S, Vt = np.linalg.svd(E)
+    # R = U @ W.T @ Vt
+    # print(R)
+    # t = U @ W @ np.diag(S) @ U.T
+    # print(t) """
+    _, R, t, mask = cv2.recoverPose(E, matched_kp1, matched_kp2, K)
+    print("R : ")
+    print(R)
+    print("t : ")
+    print(t)
+    print()
 
-# # Find epilines corresponding to points in right image (second image) and
-# # drawing its lines on left image
-# lines1 = cv.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
-# lines1 = lines1.reshape(-1,3)
-# img5,img6 = drawlines(img1,img2,lines1,pts1,pts2)
-# # Find epilines corresponding to points in left image (first image) and
-# # drawing its lines on right image
-# lines2 = cv.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
-# lines2 = lines2.reshape(-1,3)
-# img3,img4 = drawlines(img2,img1,lines2,pts2,pts1)
-# plt.subplot(121),plt.imshow(img5)
-# plt.subplot(122),plt.imshow(img3)
-# plt.show()
+    E, mask = cv2.findEssentialMat(matched_kp1, matched_kp2, K, cv2.RANSAC)
+    print("********* OpenCV ***********")
+    print("E : ")
+    print(E)
+    _, R, t, mask = cv2.recoverPose(E, matched_kp1, matched_kp2, K)
+    print("R : ")
+    print(R)
+    print("t : ")
+    print(t)
+    # W = np.array([[0.0,-1.0,0.0], [1.0,0.0,0.0], [0.0,0.0,1.0]])
+    # U, S, Vt = np.linalg.svd(E)
+    # R = U @ W.T @ Vt
+    # print(R)
+
+    plt.subplot(121),plt.imshow(img5)
+    plt.subplot(122),plt.imshow(img6)
+    plt.show()
+
+    outmatches = cv2.drawMatches(img1, kp1, img2, kp2, matches, np.array([]))
+    cv2.imshow("", outmatches)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
